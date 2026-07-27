72a767838e1e0a5f8e3c50c9d9108c50504ad59f
GOVIND SINGH <govindsingh455545@gmail.com>
2026-07-27 21:22:36 +0530

    Implement combinationSum method with backtracking

---
+++ 01_code/Combinationsum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] arr, int target, int index, List<Integer> current,
+                           List<List<Integer>> result){
+                    if(target == 0){
+                        result.add(new ArrayList<>(current));
+                    }
+                    if(target < 0) return;
+                    for(int i = index; i < arr.length; i++){
+                        current.add(arr[i]);  
+                        backtrack(arr, target - arr[i], i, current, result);
+                        current.remove(current.size() - 1); 
+                    }
+    }
+}
