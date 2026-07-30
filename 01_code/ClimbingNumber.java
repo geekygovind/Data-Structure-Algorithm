bdbe27a04b75e4c1a806098086cf6b11d4e6a75f
GOVIND SINGH <govindsingh455545@gmail.com>
2026-07-30 21:39:23 +0530

    Implement climbing stairs solution in ClimbingNumber.java

---
+++ 01_code/ClimbingNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev=1;
+        int curr = 1;
+        int c=0;
+
+        if(n==0 || n==1) return 1;
+
+        for(int i = 2; i <= n; i++){
+            int temp = curr;
+            curr += prev;
+            prev = temp;
+        }
+        return curr;
+    }
+}
