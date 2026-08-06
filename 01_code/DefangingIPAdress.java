6b78f4d30891a9acd00c39a1e362c3dafda31245
GOVIND SINGH <govindsingh455545@gmail.com>
2026-08-06 20:26:31 +0530

    Implement defangIPaddr method in Solution class

---
+++ 01_code/DefangingIPAdress.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : address.toCharArray()){
+            if(c == '.') sb.append("[.]");
+            else sb.append(c);
+           // sb.append((c== '.') ? "[.]" : c);
+        }
+        return sb.toString();
+    }
+}
