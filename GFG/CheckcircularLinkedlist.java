dc165cc057f4307eb987c6fbeb93214497dc726b
GOVIND SINGH <iamgovindsingh.ai@gmail.com>
2026-08-28 22:13:32 +0530

    Add isCircular method for circular linked list check
    Implement isCircular method to check if a linked list is circular.

---
+++ GFG/CheckcircularLinkedlist.java
@@ -0,0 +1,24 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+class Solution {
+    boolean isCircular(Node head) {
+        // code here
+        if(head == null) return true;
+        
+        Node temp = head.next;
+        while(temp != null && temp != head) temp = temp.next;
+        
+        if(temp == head) return true;
+        return false;
+    }
+}
