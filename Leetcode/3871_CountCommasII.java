ab23802a4c270ff6f6b0f5acfd16a9a71c3b659f
GOVIND SINGH <iamgovindsingh.ai@gmail.com>
2026-09-09 23:15:10 +0530

    Add countCommas method to Solution class

---
+++ Leetcode/3871_CountCommasII.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long countCommas(long n) {
+        // if(n < 1000) return 0;
+        // return n - 999;
+
+        long c = 0;
+        for(long i = 1000; i <= n; i *= 1000){
+            c += n-i +1;
+        }
+        return c;
+    }
+}
